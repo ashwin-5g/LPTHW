dc3b0b40d70b9dda273e29d557c26ddda873ef27
ashwin-5g <ashwin@5gindia.net>
2015-04-22 15:14:05 +0530

    commiting ex1 to ex6

---
+++ ex1.py
@@ -0,0 +1,11 @@
+print "Hello World!"
+print "Hello Again"
+# print ("I like typing this.")
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "i 'really' want you to go there" ' d is "d"'
+print
+# print ("this won't get printed very likely")
+
---
+++ ex3.py
@@ -0,0 +1,32 @@
+#prompt for chicken count
+print "I will now count my chickens:"
+#display hens' count
+print "Hens", 25 + 30 / 6
+#display roosters' count
+print "Roosters", 100 - 25 * 3
+% 4
+
+#display eggs' count
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#comparison operation in use
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+#addition
+print "What is 3 + 2?", 3 + 2
+#subtraction
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+print "How about some more."
+#greater than function
+print "Is it greater?", 5 > -2
+#greater than or equal function
+print "Is it greater or equal?", 5 >= -2 
+#less than or equal function
+print "Is it less or equal?", 5 <= -2
+
+
